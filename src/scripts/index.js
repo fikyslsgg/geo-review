301e9277cc6eb7f137ad9aed3f7d8d1c2b9ffee2
Yura Sidyakin <yurafikys@gmail.com>
2022-12-23 23:00:19 +0300

    Добвил baloon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
+const reviews = []
+
+
 document.addEventListener('DOMContentLoaded' , (e)=> {
     ymaps.ready(init);
+
     function init() {
       const myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
@@ -17,15 +21,84 @@ document.addEventListener('DOMContentLoaded' , (e)=> {
     }
 });
 
-function openBallon(map, coords) {
-  map.balloon.open(coords, {
+function getOptionsCluster(coords) {
+  const clusterObjects = [];
+
+  for (const review of reviews) {
+    if (JSON.stringify(review.coords) ===  JSON.stringify(coords)) {
+      const geoObj = new ymaps.GeoObject({
+        geometry:{type:'Point', coordinates:coords}
+      }) 
+      clusterObjects.push(geoObj)
+    }
+  }
+  return clusterObjects
+}
+
+function addCluster(map, coords) {
+  const clusterer = new ymaps.Clusterer({clusterDisableClickZoom: true})
+  clusterer.options.set('hasBalloon', false)
+
+  function addToCluster() {
+    const myGoeObjects = getOptionsCluster(coords)
+    clusterer.add(myGoeObjects)
+    map.geoObjects.add(clusterer)
+    map.balloon.close()
+  }
+
+clusterer.events.add('click', function(e) {
+  e.preventDefault();
+  openBallon(map, coords, clusterer, addToCluster)
+})
+
+  addToCluster()
+}
+
+function getReviewList (coords) {
+  let reviewListHTML = ''
+
+  for (const review of reviews) {
+    if (JSON.stringify(review.coords) ===  JSON.stringify(coords)){
+      reviewListHTML += `
+      <div class = 'review'>
+        <div> <strong>Место: </strong>${review.place}</div>
+        <div> <strong>Автор: </strong>${review.author}</div>
+        <div> <strong>Отзыв: </strong>${review.comment}</div>
+      </div>
+      `
+    }
+  }
+  return reviewListHTML
+}
+
+async function openBallon(map, coords, clusterer, fn) {
+  await map.balloon.open(coords, {
     content: `
+    <div class= 'reviews'>${getReviewList(coords)}</div>
     <form id = "add-form">
-    <input type = "text" placeholder = "Названгие места" name = "place"><br></br>
-    <input type = "text" placeholder = "Вашк имя" name = "author"><br></br>
-    <textarea placeholder = "Ваш отзыв" name = "revirew"></textarea><br></br>
-    <button id = "add-btn">Добавить</button><br></br>
+      <input type = "text" class='place' placeholder = "Название места" name = "place"><br></br>
+      <input type = "text" class='author' placeholder = "Ваше имя" name = "author"><br></br>
+      <textarea placeholder = "Оценка" class = 'comment' ></textarea><br></br>
+      <button id = "add-btn">Добавить</button><br></br>
     </form>
     `
+  })
+  document.querySelector('#add-form').addEventListener('submit', (e)=> {
+    e.preventDefault();
+    if (clusterer) {
+      clusterer.removeAll()
+    }
+    let place = document.querySelector('.place').value;
+    let author = document.querySelector('.author').value;
+    let comment = document.querySelector('.comment').value;
+    reviews.push({
+      coords:coords,
+      place:place,
+      author:author,
+      comment: comment
+    })
+
+    !fn ? addCluster(map,coords) : fn()
+    map.balloon.close()
   })
 }
