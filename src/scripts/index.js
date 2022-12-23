dc9f54cfba0cfa14aa28c7da0a6faa5a8681da72
Yura Sidyakin <yurafikys@gmail.com>
2022-12-23 13:05:38 +0300

    Добавил открытие baloon

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,8 +3,29 @@ document.addEventListener('DOMContentLoaded' , (e)=> {
     function init() {
       const myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
-        zoom: 14
+        zoom: 14,
+        controls:[]
     });
-    }
 
+    myMap.events.add('click', function(e) {
+      const coords = e.get('coords')
+
+      openBallon(myMap, coords)
+    })
+
+
+    }
 });
+
+function openBallon(map, coords) {
+  map.balloon.open(coords, {
+    content: `
+    <form id = "add-form">
+    <input type = "text" placeholder = "Названгие места" name = "place"><br></br>
+    <input type = "text" placeholder = "Вашк имя" name = "author"><br></br>
+    <textarea placeholder = "Ваш отзыв" name = "revirew"></textarea><br></br>
+    <button id = "add-btn">Добавить</button><br></br>
+    </form>
+    `
+  })
+}
